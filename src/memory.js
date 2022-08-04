29056dd85c2beed8c10dd5f3beba3859a792df8d
Julien <benedetjulien@gmail.com>
2022-08-04 14:02:54 +0200

    Not started dom

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,39 @@
 class MemoryGame {
   constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
+    this.cards = cards
+    this.pickedCards = []
+    this.pairsClicked = 0
+    this.pairsGuessed = 0
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards) {
+      return undefined;
+    }
+    for(let i = 0; i < this.cards.length; i++) {
+      let index = Math.floor(Math.random() * this.cards.length);
+      let temp = this.cards[i];
+      this.cards[i] = this.cards[index];
+      this.cards[index] = temp;
+    }
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    const pair = this.cards.length / 2;
+    if (this.pairsGuessed === pair) {
+      return true;
+    }
+    return false;
   }
 }
